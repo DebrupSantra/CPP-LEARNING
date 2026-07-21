fc141abd9edb3ea15a6ee7311a94d8d946794cc5
DebrupSantra <debrup.santra@gmail.com>
2026-07-21 10:28:33 +0530

    Added Sum of Array Elements using pointers

---
+++ SumOfArrayElements(Pointers).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[5]={10,20,30,40,50};
+    int *ptr=arr;
+    int sum=0;
+    for(int i=0;i<5;i++)
+    {
+        sum += *(ptr+i);
+    }
+    cout<<"Sum = "<<sum;
+}
